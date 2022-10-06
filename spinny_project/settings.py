847bb7e398d8054520f4589074efe43e15518dc6
drono07 <86304881+drono07@users.noreply.github.com>
2022-10-06 23:06:27 +0530

    Update settings.py

--- spinny_project/settings.py
+++ spinny_project/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
