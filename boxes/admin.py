de05caa3413593ea75c2e97acbbae9e0f7c89cc8
drono07 <86304881+drono07@users.noreply.github.com>
2023-04-12 03:10:13 +0530

    Update admin.py
    update Config Admin

--- boxes/admin.py
+++ boxes/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Boxes
+from .models import Boxes , Config
 
 # Register your models here.
 
 
 
 class BoxesAdmin(admin.ModelAdmin):
-    list_display = ['length', 'breadth','height','created_by','area','volume']
-    search_fields =['length', 'breadth','height','created_by','area','volume']
+    list_display = ['length', 'breadth','height','created_by']
+    search_fields =['length', 'breadth','height','created_by']
 
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ['average_area','average_volume','total_boxes','total_boxes_user','active']
+    search_fields = ['average_area','average_volume','total_boxes']
 
 admin.site.register(Boxes, BoxesAdmin)
+admin.site.register(Config , ConfigAdmin)
+
