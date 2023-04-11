2f0b0835fdb8552c253ec24ba71ea11f54c96e11
drono07 <86304881+drono07@users.noreply.github.com>
2023-04-12 03:11:12 +0530

    Update filters.py
    Seperate My box Filters

--- boxes/filters.py
+++ boxes/filters.py
@@ -12,7 +12,19 @@ class BoxFilter(django_filters.FilterSet):
     area_lt = django_filters.NumberFilter(field_name="area", lookup_expr="lt")
     volume_gt = django_filters.NumberFilter(field_name="volume", lookup_expr="gt")
     volume_lt = django_filters.NumberFilter(field_name="volume", lookup_expr="lt")
+    created_on_before = django_filters.DateFilter(field_name="created_on",lookup_expr="lt")
+    created_on_after = django_filters.DateFilter(field_name="created_on",lookup_expr="gt")
 
     class Meta:
         model=Boxes
-        fields=['length','breadth', 'height', 'area','volume','length_gt','area_gt','length_lt','area_lt','heigth_gt','heigth_lt','breadth_gt','breadth_gt', 'volume_gt', 'volume_lt']
+        fields=['length','breadth', 'height', 'area','volume','length_gt','area_gt'\
+                ,'length_lt','area_lt','heigth_gt','heigth_lt','breadth_gt','breadth_gt',\
+                'volume_gt', 'volume_lt',"created_by","created_on_before","created_on_after"]
+        
+class MyBoxFilter(BoxFilter):
+
+    class Meta:
+        model=Boxes
+        fields=['length','breadth', 'height', 'area','volume','length_gt','area_gt'\
+                ,'length_lt','area_lt','heigth_gt','heigth_lt','breadth_gt','breadth_gt',\
+                'volume_gt', 'volume_lt']
