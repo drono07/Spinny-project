f5729c93c231f66bf15fa2834e2029ff8b9a7c90
drono07 <86304881+drono07@users.noreply.github.com>
2023-04-11 18:11:30 +0530

    Update utils.py
    removing functionality of volume and area

--- boxes/utils.py
+++ boxes/utils.py
@@ -5,21 +5,13 @@ from rest_framework.response import Response
 class BoxUtils:
 
     @classmethod
-    def calculate_area_volume(cls, length, breadth, height):
-        area = 2 * (length * breadth + breadth * height + length * height)
-        volume = length * breadth * height
-        return area, volume
-
-    @classmethod
     def create_box(cls, length, breadth, height, user):
-        area, volume = cls.calculate_area_volume(length=length, breadth=breadth, height=height)
         box=Boxes.objects.create(
             length=length,
             breadth=breadth,
             height=height,
-            area=area,
-            volume=volume,
-            created_by=user,
+            created_by=user
         )
+        box.save()
         return box,"Success"
 
